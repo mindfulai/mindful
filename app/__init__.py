c53bf1a81b0694e0b719c9d7d2e0b312f623ff2a
Dawn <xuedonglin1994@gmail.com>
2018-11-13 22:39:39 +0800

    feat: twitter_blueprint url prefix

--- app/__init__.py
+++ app/__init__.py
@@ -19,5 +19,5 @@ login_manager.init_app(app)
 
 from app.views import facebook_blueprint, twitter_blueprint
 
-app.register_blueprint(twitter_blueprint)
+app.register_blueprint(twitter_blueprint, url_prefix="/login")
 app.register_blueprint(facebook_blueprint, url_prefix="/login")
