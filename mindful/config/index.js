05309d1581a002bf04f48471f0f1406248f82e52
lianghongmin <159.1429204@qq.com>
2018-12-05 12:07:30 +0800

    merge

--- mindful/config/index.js
+++ mindful/config/index.js
@@ -28,7 +28,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    //host: '192.168.1.211', // can be overwritten by process.env.HOST
+    //host: '192.168.1.111', // can be overwritten by process.env.HOST
     //host: '127.0.0.1',
     host: 'localhost',
     port: 8000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
